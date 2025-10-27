6462a41f96b956feaafed604c64815c13d0595cf
paulogtl <paulopaulogtl@gmail.com>
2025-10-27 20:23:24 -0300

    Delete Try-Except directory

--- Try-Except/ex5.py
+++
@@ -1,9 +0,0 @@
-try:
-    saldo = float(input("Digite o saldo da conta: "))
-    valor = float(input("Digite o valor da transferência: "))
-    if valor > saldo:
-        raise ValueError("Saldo insuficiente.")
-    saldo -= valor
-    print(f"Transferência realizada com sucesso. Novo saldo: R${saldo:.2f}")
-except ValueError as e:
-    print(f"Erro: {e}")
